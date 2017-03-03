5676145cebfb6c2c59838f2ab2d1c19b996887a7
umaramasub <uramasubramanian@usfca.edu>
2017-03-03 13:51:08 -0800

    Add function to read ECG file

--- viz.py
+++ viz.py
@@ -125,8 +125,35 @@ def plot(data):
     plt.ylabel('LOS')
     plt.legend(loc='lower left')
 
+def readCSV_ecg(filepath):
+    """filepath is provided as a string representing an absolute path to the CSV
+    file. The function will open this file, read in the contents, close the
+    file, and then return the contents as a dataframe
+    :param filepath:string as the absolute path to the file
+    :return dataframe
+    """
+
+    # Normalize the file path to work on any system
+    norm_filepath = os.path.normpath(filepath)
+
+    # Open the file for reading
+    file = open(norm_filepath, 'r')
+
+    # Generate a data frame from the text and skip the first column as it is header
+    data = pd.read_csv(norm_filepath, delimiter=',', header=None, na_values='?', names=[
+        'survival', 'still-alive', 'age-at-heart-attack', 'pericardial effusion',
+        'fractional-shortening', 'epss', 'lvdd', 'wall-motion-score', 'wall-motion-index', 'mult', 'name', 'group',
+        'alive-at-1'])
+    file.close()
+    return data
+
+
+
+
+
 
 if __name__ == '__main__':
     data = readCSV("data.csv")
     plot(data)
     plt.show()
+
